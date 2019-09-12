6b7763d0da68689ee78337e5756fe7f8516a293d
TaniaAlex <2tatyanan@gmail.com>
2019-09-12 22:40:25 +0200

    setInterval(), setTimout(), clearInterval() Challenge Completed

--- index.js
+++ index.js
@@ -13,3 +13,27 @@
  * "Here is message number 4"
  * "Here is message number 5"
  */
+
+ /**
+  * SOLUTION 1 (my)
+  */
+let count = 1;
+let i = 1;
+const printBlogPost = setInterval( function(){
+        console.log("Here is blog post number " + i);
+        i = i + 1;
+        if(count >= 5) clearInterval(printBlogPost);
+        count++;
+}, 2000);
+
+/**
+ * SOLUTION 2 (TUTOR)
+ */
+let t = 1;
+const printMsg = setInterval( function(){
+    console.log("Here is message number " + t);
+    t = t + 1;
+    } ,2000);
+setTimeout( function(){ 
+    clearInterval(printMsg); 
+    }, 10000);
