ecf9620970552cac4cc24d30036c8ea2ce123720
TaniaAlex <2tatyanan@gmail.com>
2019-09-19 21:36:33 +0200

    Added examples

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 //  * 
 //  * "if" statement
 //  */
-// let i = 10;
+let i = 10;
 
-// if (i > 0) {
-//   console.log(i);
-// }
+if (i > 0) {
+  console.log(i);
+}
 
-// if (i) {
-//   console.log(i);
-// }
+if (i) {
+  console.log(i);
+}
 
 
 // /**
@@ -19,30 +19,52 @@
 //  * 
 //  * "if else" statement
 //  */
-// const myArray = [4, true, "abc", null, 5, 10];
+const bookstore = ["dvg", "ghj", "yui", "pol", "kol", "hyg","abc"];
+if (bookstore.length < 5){
+    console.log("Klein, aber sehr gemuetlich!")
+}else{
+    console.log("Die besten Bücher für jedes Geschmack!")
+}
 
-// if (myArray.length > 5) {
-//   console.log("Array is large");
-// } else {
-//   console.log("Array is small");
-// }
+
+const myArray = [4, true, "abc", null, 5, 10];
+
+if (myArray.length > 5) {
+  console.log("Array is large");
+} else {
+  console.log("Array is small");
+}
 
 // /**
 //  * EXAMPLE 3
 //  * 
 //  * Chaining "if" and "if else" statements
 //  */
-// let color = "green";
+let obst = "Salami";
 
-// if (color === "green") {
-//   console.log("It is green color");
-// } else if (color === "yellow") {
-//   console.log("It is yellow color");
-// } else if (color === "red") {
-//   console.log("It is red color");
-// } else {
-//   console.log("Color is unknown");
-// }
+if (obst === "Apfel") {
+  console.log("Ich liebe Apfelkuchen");
+} else if (obst === "Kirschen") {
+  console.log("Ich liebe Schwarzwaelder Kirschtorte");
+} else if (obst === "Aprikose") {
+  console.log("Ich liebe Aprikosenkuchen");
+} else {
+  console.log("Pizza ist die beste Torte");
+}
+
+
+let color = "yellow";
+
+if (color === "green") {
+  console.log("It is green color");
+} else if (color === "yellow") {
+  console.log("It is yellow color");
+} else if (color === "red") {
+  console.log("It is red color");
+} else {
+  console.log("Color is unknown");
+}
+
 
 
 // /**
@@ -71,13 +93,35 @@
 //  * 
 //  * Ternary operator
 //  */
-// let i = 10;
-// let j;
 
-// j = i < 100 ? i : 0;
+let age = 21
+voteable = (age < 18) ? "Too young" : "Old enough";
+console.log(voteable);
 
-// i > 10 && j
-//   ? console.log("Condition is truthy")
-//   : console.log("Condition is falsy");
 
-// console.log(j);
+// -----------------
+function getFee(isMember) {
+    return (isMember ? "$2.00" : "$10.00");
+  }
+  
+  console.log(getFee(true));
+  // expected output: "$2.00"
+  
+  console.log(getFee(false));
+  // expected output: "$10.00"
+  
+  console.log(getFee(1));
+  // expected output: "$2.00"
+
+
+  //-------------------
+let s = 10;
+let j;
+
+j = s < 100 ? s : 0;
+
+s > 10 && j
+  ? console.log("Condition is truthy")
+  : console.log("Condition is falsy");
+
+console.log(j);
