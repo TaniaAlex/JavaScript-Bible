aa570cfc8a520be38c645c6ae0db564e2c61ef7f
TaniaAlex <2tatyanan@gmail.com>
2019-09-19 16:02:56 +0200

    Challenge completed, added new examples

--- index.js
+++ index.js
@@ -5,11 +5,17 @@
 //  * First add string "Hello"
 //  * Second add number 100
 //  */
+
+// const briefcase = [true, 3];
+// briefcase.unshift("Lock");
+// briefcase.unshift(234);
+// console.log(briefcase);
 // // const myArray = [true, null];
 
 // // console.log(myArray); // [100, "Hello", true, null]
 
 // // SOLUTION
+
 // const myArray = [true, null];
 
 // myArray.unshift("Hello");
@@ -27,6 +33,14 @@
 //  * Log to the console Array and explain results.
 //  * What is the length of the final Array?
 //  */
+
+// const notes = [1, 1];
+// console.log(notes); // (2) [1, 1]
+
+// notes[10] = "sehr gut";
+// console.log(notes); // (11) [1, 1, empty × 8, "sehr gut"]
+// console.log(notes.length); // 11
+
 // // const myArray = [1, 2];
 
 // // SOLUTION
@@ -50,6 +64,45 @@
 //  * Each object will have two properties - carBrand, price.
 //  * Add one more object to the Array
 //  */
+
+// const shoes = [
+//     {
+//         shoesBrand: "Remonte",
+//         price: 110
+//     },
+//     {
+//         shoesBrand: "Gabor",
+//         price: 120
+//     },
+//     {
+//         shoesBrand: "Puma",
+//         price: 100
+//     }
+// ]
+
+// console.log(shoes); //  [{…}, {…}, {…}]
+//                     // 0: {shoesBrand: "Remonte", price: 110}
+//                     // 1: {shoesBrand: "Gabor", price: 120}
+//                     // 2: {shoesBrand: "Puma", price: 100}
+//                     // 3: {shoesBrand: "Rieker", price: 50}
+//                     // length: 4
+//                     // __proto__: Array(0)
+// const newShoes = {
+//     shoesBrand: "Rieker",
+//     price: 50
+// };
+// shoes.push(newShoes);
+// console.log(shoes); // (4) [{…}, {…}, {…}, {…}]
+                    // 0: {shoesBrand: "Remonte", price: 110}
+                    // 1: {shoesBrand: "Gabor", price: 120}
+                    // 2: {shoesBrand: "Puma", price: 100}
+                    // 3: {shoesBrand: "Rieker", price: 50}
+                    // length: 4
+                    // __proto__: Array(0)
+
+
+
+
 // const arrayOfCars = [
 //   {
 //     carBrand: "Toyota",
