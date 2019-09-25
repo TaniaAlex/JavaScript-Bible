e1aecde07d6a1a089dd43ae92f1abd84f2a9c019
TaniaAlex <2tatyanan@gmail.com>
2019-09-25 11:03:34 +0200

    Added Comments

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // Callbacks, events, events queue, execution contexts stack, Web APIs
+
+// CALLBACK is a function passed as argument in the call to other function
 function waitingFn(timeInMs) {
   const futureTime = Date.now() + timeInMs;
   while (futureTime > Date.now()) {
     // waiting...
   }
 }
+
+// setTimeOut is one of the global methods ->
+// setTimeOut is a propertu of the global Object Window
+// window.setTimeOut
+//setTimeOut requires 2 arguments -> callback function and timeout in ms
 
 setTimeout(() => console.log("Callback is executed"), 0);
 
